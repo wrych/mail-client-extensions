174a0a918be4f30459acc6770d2d435f409cc77c
Andy Moesch <andy.moesch@dectris.com>
2023-04-19 10:21:18 +0200

    fix login

--- gmail/src/views/login.js
+++ gmail/src/views/login.js
@@ -1,15 +1,15 @@
 function onNextLogin(event) {
-    var validatedUrl = (0, format_1.formatUrl)(event.formInput.odooServerUrl);
+    var validatedUrl = (0, formatUrl)(event.formInput.odooServerUrl);
     if (!validatedUrl) {
-        return (0, helpers_1.notify)("Invalid URL");
+        return (0, notify)("Invalid URL");
     }
     if (!/^https:\/\/([^\/?]*\.)?odoo\.com(\/|$)/.test(validatedUrl)) {
-        return (0, helpers_1.notify)("The URL must be a subdomain of odoo.com");
+        return (0, notify)("The URL must be a subdomain of odoo.com");
     }
-    (0, translation_1.clearTranslationCache)();
+    (0, clearTranslationCache)();
     State.odooServerUrl = validatedUrl;
-    if (!(0, odoo_auth_1.isOdooDatabaseReachable)(validatedUrl)) {
-        return (0, helpers_1.notify)(
+    if (!(0, isOdooDatabaseReachable)(validatedUrl)) {
+        return (0, notify)(
             "Could not connect to your database. Make sure the module is installed in Odoo (Settings > General Settings > Integrations > Mail Plugins)",
         );
     }
@@ -41,19 +41,15 @@ function buildLoginMainView() {
             )
             .addWidget(
                 CardService.newTextButton()
-                    .setText(
-                        (0, format_1.repeat)(invisibleChar, 12) + "Login" + (0, format_1.repeat)(invisibleChar, 12),
-                    )
+                    .setText((0, repeat)(invisibleChar, 12) + "Login" + (0, repeat)(invisibleChar, 12))
                     .setTextButtonStyle(CardService.TextButtonStyle.FILLED)
                     .setBackgroundColor("#00A09D")
                     .setOnClickAction(CardService.newAction().setFunctionName("onNextLogin")),
             )
-            .addWidget(CardService.newTextParagraph().setText((0, format_1.repeat)(invisibleChar, 13) + "<b>OR</b>"))
+            .addWidget(CardService.newTextParagraph().setText((0, repeat)(invisibleChar, 13) + "<b>OR</b>"))
             .addWidget(
                 CardService.newTextButton()
-                    .setText(
-                        (0, format_1.repeat)(invisibleChar, 11) + " Sign Up" + (0, format_1.repeat)(invisibleChar, 11),
-                    )
+                    .setText((0, repeat)(invisibleChar, 11) + " Sign Up" + (0, repeat)(invisibleChar, 11))
                     .setOpenLink(
                         CardService.newOpenLink().setUrl(
                             "https://www.odoo.com/trial?selected_app=mail_plugin:crm:helpdesk:project",
@@ -61,39 +57,33 @@ function buildLoginMainView() {
                     ),
             )
             .addWidget(
-                (0, helpers_1.createKeyValueWidget)(
+                (0, createKeyValueWidget)(
                     null,
                     "Create leads from emails sent to your email address.",
                     IMAGES_LOGIN.email,
                 ),
             )
             .addWidget(
-                (0, helpers_1.createKeyValueWidget)(
+                (0, createKeyValueWidget)(
                     null,
                     "Create tickets from emails sent to your email address.",
                     IMAGES_LOGIN.ticket,
                 ),
             )
+            .addWidget((0, createKeyValueWidget)(null, "Centralize Prospects' emails into CRM.", IMAGES_LOGIN.crm))
             .addWidget(
-                (0, helpers_1.createKeyValueWidget)(null, "Centralize Prospects' emails into CRM.", IMAGES_LOGIN.crm),
-            )
-            .addWidget(
-                (0, helpers_1.createKeyValueWidget)(
+                (0, createKeyValueWidget)(
                     null,
                     "Generate Tasks from emails sent to your email address in any Odoo project.",
                     IMAGES_LOGIN.project,
                 ),
             )
             .addWidget(
-                (0, helpers_1.createKeyValueWidget)(
-                    null,
-                    "Search and store insights on your contacts.",
-                    IMAGES_LOGIN.search,
-                ),
+                (0, createKeyValueWidget)(null, "Search and store insights on your contacts.", IMAGES_LOGIN.search),
             )
             .addWidget(
                 CardService.newTextParagraph().setText(
-                    (0, format_1.repeat)(invisibleChar, 13) + '<a href="'.concat(faqUrl, '">FAQ</a>'),
+                    (0, repeat)(invisibleChar, 13) + '<a href="'.concat(faqUrl, '">FAQ</a>'),
                 ),
             ),
     );
